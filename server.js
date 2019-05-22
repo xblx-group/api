4f36812e3033d02b14fdcb89bb141f23bcddb6df
TTaaaaRRR <fedor.r-n@yandex.ru>
2019-05-22 13:14:46 +0300

    added login, getUser, ping

--- server.js
+++ server.js
@@ -1,10 +1,50 @@
 var express = require('express');
 var app = express();
+var masterPass = 0; //put your NUMERICAL pass
 
-app.get('/', function (req, res) {
-  res.send('{"OK"}');
+var users = [
+{
+	XblXtoken: 0, //Test user
+	VKtoken: ""}
+];
+
+function genToken(){
+	//TODO
+	return users.size();
+};
+
+app.get('/login', function (req, res) {
+	vk = req.param("VKtoken");
+	xblx = req.param("XblXtoken");
+	
+	users.forEach(function(a, b, c){
+		if(a.XblXtoken == xblx){
+			res.send("OK");
+			}
+		}
+	);
+	res.send("ERR " + xblx);
+	
 });
+app.get('/getUser', function (req, res) {
+	id = req.param('id');
+	res.send(users[id]);
+});
+	
+	
+	
+app.get('/reg', function (req, res) {
+	mp = req.param("Pass");
+	if(mp == masterPass){
+		resu += genToken();
+		users.add({XblXtoken: genToken(), VKtoken: ""});
+	}
+	res.send("OK");
+});
+		
+
+app.get('/', function (req, res) {res.send('pong');}); 
 
 app.listen(8000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('working...');
 });
